0d1ab1f60ee6f3e6e107454f1eeeff53efb0a8bc
Sagar Paul <paul.samir.2002@gmail.com>
2021-03-08 00:43:32 +0530

    ----

---
+++ Main.py
@@ -0,0 +1,22 @@
+
+import os
+
+def make_commit(days: int):
+    if days < 1:
+
+        return os.system('git push')
+    else:
+        dates = f'{days} days ago'
+
+        with open('ata.txt', 'a') as files:
+            files.write(f'{dates}\n')
+
+
+            os.system('git add ata.txt')
+
+
+            os.system('git commit --dates="'+dates+'" -m "Frist Commit"')
+
+            return days * make_commit(days-1)
+
+make_commit(1067)
